594fd3a2fab7631f6a17522bfa21d1ca7dff1a5f
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-08-25 11:50:39 +0200

    Add an option to open links in a new group

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,67 +1,77 @@
-import {LinksResponse} from "./models/links-response";
-import {FromBackgroundScript} from "./utils/communication";
+import { LinksResponse } from "./models/links-response";
+import { FromBackgroundScript } from "./utils/communication";
 
 let contextMenuEnabled = false;
 
 FromBackgroundScript.listenToContentScript("ENABLE_CONTEXT_MENU", () => {
-    setupContextMenu();
-    return Promise.resolve();
+  setupContextMenu();
+  return Promise.resolve();
 });
 
 FromBackgroundScript.listenToContentScript("DISABLE_CONTEXT_MENU", () => {
-    tearDownContextMenu();
-    return Promise.resolve();
+  tearDownContextMenu();
+  return Promise.resolve();
 });
 
 setUpClickListener();
 
 function setUpClickListener() {
-    chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab) => {        
-        (async () => {
-            const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-            
-            const response = await FromBackgroundScript.sendToContentScript<LinksResponse>(tab.id, "CONTEXT_MENU_CLICKED");
+  chrome.contextMenus.onClicked.addListener(
+    (info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab) => {
+      (async () => {
+        const [tab] = await chrome.tabs.query({
+          active: true,
+          lastFocusedWindow: true,
+        });
 
-            if (response?.success !== true) {
-                console.error("Content script had some issues retrieving links");
-            } else {
-                openNewTabs(response.links);
-            }
-        })();
+        const response =
+          await FromBackgroundScript.sendToContentScript<LinksResponse>(
+            tab.id,
+            "CONTEXT_MENU_CLICKED"
+          );
 
-        return true;
-    });
+        if (response?.success !== true) {
+          console.error("Content script had some issues retrieving links");
+        } else {
+          await openNewTabs(response.links);
+        }
+      })();
+
+      return true;
+    }
+  );
 }
 
-
 function setupContextMenu() {
-    if (contextMenuEnabled) {
-        return;
-    }
-    
-    chrome.contextMenus.create({
-        id: "1",
-        title: "Open links in new tabs",
-        contexts: ['selection']
-    });
+  if (contextMenuEnabled) {
+    return;
+  }
 
-    contextMenuEnabled = true;
+  chrome.contextMenus.create({
+    id: "1",
+    title: "Open links in new tabs",
+    contexts: ["selection"],
+  });
+
+  contextMenuEnabled = true;
 }
 
 function tearDownContextMenu() {
-    if (contextMenuEnabled) {
-        chrome.contextMenus.removeAll();
-        contextMenuEnabled = false;
-    }
+  if (contextMenuEnabled) {
+    chrome.contextMenus.removeAll();
+    contextMenuEnabled = false;
+  }
 }
 
-function openNewTabs(links: string[]) {
-    links.map(url => {
-        chrome.tabs.create(
-            {
-                active: false,
-                url
-            }
-        );
-    });
+async function openNewTabs(links: string[]) {
+  const tabs = await Promise.all(
+    links.map((url) =>
+      chrome.tabs.create({
+        active: false,
+        url,
+      })
+    )
+  );
+
+  await chrome.tabs.group({ tabIds: tabs.map((tab) => tab.id) });
 }
